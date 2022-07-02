8e6d819bd8f366ebcb44ac5f4fad65754929d5b1
nick.gourley@gmail.com <nick.gourley@gmail.com>
2022-07-02 17:02:51 -0400

    init adfish emergency replacement

---
+++ components/Layout.js
@@ -0,0 +1,15 @@
+// components/layout.js
+import styles from '../styles/Home.module.css'
+// import Navbar from './navbar'
+// import Footer from './footer'
+
+export default function Layout({ children }) {
+  return (
+    <>
+      {/* <Navbar /> */}
+      <main className={styles.main}>
+{children}</main>
+      {/* <Footer /> */}
+    </>
+  )
+}
